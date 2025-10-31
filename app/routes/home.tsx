16b2aa192f9dcb8255b11f49fb3601baff3ebf35
Sajid <shaikatahmed78@gmail.com>
2025-11-01 04:58:51 +0600

    feat: implement dynamic ApplicantCard component and integrate it into Home page layout
    - Created fully responsive ApplicantCard component to display resume data dynamically
    - Integrated RatingCircle visualization for overall feedback score

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "~/constants/resumeData";
+import ApplicantCard from "~/components/ApplicantCard";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -19,13 +21,19 @@ export default function Home() {
       <Navbar />
 
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading  py-16">
           <h1>Monitor Your Resume & Application Performance</h1>
           <h2>Let AI guide you to better resumes and career opportunities</h2>
         </div>
-      </section>
 
-      
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ApplicantCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
     </main>
   );
 }
